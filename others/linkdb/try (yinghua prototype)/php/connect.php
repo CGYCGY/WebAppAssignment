29724d30b7ba94d5023f8f21c07ab4dd86e4fe22
Cheong Gwen Yit <cgyyit@gmail.com>
2018-01-19 04:09:59 +0800

    -making the browse button works like it was, navigate to browse.html -add a button to navigate to apply ticket screen
    -add new working post function for linking register.html to db, will add another for login.html in the next commit

---
+++ others/linkdb/try (yinghua prototype)/php/connect.php
@@ -0,0 +1,47 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+function dataQuery($query, $param) {
+    // database info
+    $hostname = "localhost";
+    $dbname = "waadb";
+    $user = "root";
+    $pass = "";
+
+    // type of query
+    $query_type = explode(' ', $query);
+
+    // establish database connection
+    try {
+        $connect = new PDO("mysql:host=$hostname;dbname=$dbname", $user, $pass);
+        $connect->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e) {
+        echo $e->getMessage();
+        // might use this later
+        $error_code = $e->getCode();
+    }
+
+    // run the query
+    try {
+        $query_result = $connect->prepare($query);
+        $query_result->execute($param);
+        if($query_result != null && 'SELECT' == $query_type[0]) {
+            $results = $query_result->fetchAll(PDO::FETCH_ASSOC);
+            return $results;
+        } else {
+            return $query_result->rowCount();
+        }
+        $query_result = null; // first of the two steps to properly close
+        $connect = null; // second step tp close the connection
+    }
+    catch(PDOException $e) {
+        $error_msg = $e->getMessage();
+        echo $error_msg;
+    }
+// return ntg back
+return null;
+}
+?>
