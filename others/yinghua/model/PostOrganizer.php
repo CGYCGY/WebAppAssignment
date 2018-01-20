719f7005cf1195e4cdf66a0e9561e3c986b76e3a
Yinghua <yinghuachai0@gmail.com>
2018-01-21 00:22:32 +0800

    created PostOrganizer and PostUser API without query exec, database connection return 200

--- others/yinghua/model/PostOrganizer.php
+++ others/yinghua/model/PostOrganizer.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 // Use MySQLi
 function post_organizer() {
-    $db = mysqli_connect('localhost','root','','waadb')
+    $db = mysqli_connect('localhost:3306','root','','waadb')
     or die('Error connecting to Web Assignment Database Server.');
 
     if(!$db){
