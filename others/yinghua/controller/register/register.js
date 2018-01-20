d6ec665492125b441e22c16d962d4ea3f28d9606
Yinghua <yinghuachai0@gmail.com>
2018-01-20 14:17:33 +0800

    password field validation complete

--- others/yinghua/controller/register/register.js
+++ others/yinghua/controller/register/register.js
@@ -2,22 +2,44 @@ $(function() {
     $('.warning_message').hide();
 });
 
+var username_regex = new RegExp('^[a-z0-9_\-]{5,50}');
+var password_regex = new RegExp('^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])\\w{8,}$');
+
+
+
 function test() {
-    var username = document.getElementById('username').value;
-    if(!validate_username(username)){
-        $('.warning_message').show();
-    }
-    else {
-        $('.warning_message').hide();
-
-    }
-
+    validate_password();
 }
 
 /**
  * The username should only allow lower case with numbers and at least 5 characters, maximum 50 characters
  */
-function validate_username (input_value) {
-    var username_regex = new RegExp('^[a-z0-9_-]{5,50}');
-    return username_regex.test(input_value);
+function validate_username() {
+    var username = document.getElementById('username').value;
+    if(!username_regex.test(username)){
+        $('#user_warning').show();
+    }
+    else {
+        $('#user_warning').hide();
+
+    }
+}
+
+function validate_password() {
+    var pw = document.getElementById('password').value;
+    var retype_pw = document.getElementById('retypePassword').value;
+    var bln_pw = password_regex.test(pw);
+
+    if(!bln_pw){
+        $('#password_warning').text('The password shall contain at least eight characters long with uppercase, lowercase and numbers. ');
+        $('#password_warning').show();
+    }
+    else if (pw !== retype_pw) {
+        $('#password_warning').text('The retype password does not match with previous password value');
+        $('#password_warning').show();
+    }
+    else if (bln_pw && pw === retype_pw){
+        console.log('correct')
+        $('#password_warning').hide();
+    }
 }
